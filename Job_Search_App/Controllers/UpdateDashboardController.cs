b8904dd132235e6f00cbad1910e2d985df4ec0d0
EmranAhmed00 <emranahmed.my@gmail.com>
2020-05-14 20:05:53 +0200

    Update_Dashboard

---
+++ Job_Search_App/Controllers/UpdateDashboardController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Job_Search_App.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Job_Search_App.ViewModels;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Job_Search_App.Controllers
+{
+    public class UpdateDashboardController : Controller
+    {
+
+        private readonly ILogger _logger;
+        private readonly ApplicationDbContext _context;
+        private UserManager<User> _userManager;
+
+        public UpdateDashboardController(ApplicationDbContext context, UserManager<User> userManager, ILogger<JobController> logger)
+        {
+            _logger = logger;
+            _context = context;
+            _userManager = userManager;
+        }
+
+
+        //// Edit Dashboard
+
+        [HttpGet]
+        [Route("employer/jobs/{id}/edit-dashboard")]
+
+        [Authorize(Roles = "Employer")]
+
+        public IActionResult EditDashboard(int id)
+
+        {
+            var model = new DashboardViewModel();
+            model.jobInfo = _context.Jobs.SingleOrDefault(k => k.Id == id);
+            return View(model);
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "Employer")]
+        [Route("employer/jobs/{id}/edit-dashboard")]
+
+        [ValidateAntiForgeryToken]
+        public IActionResult EditDashboard(DashboardViewModel model, int id)
+
+        {
+
+            var newCus = _context.Jobs.SingleOrDefault(k => k.Id == id);
+
+            if (model.jobInfo == null)
+            {
+                model.jobInfo = newCus;
+            }
+            else
+            {
+                newCus.Title = model.jobInfo.Title;
+                newCus.Type = model.jobInfo.Type;
+
+                newCus.Description = model.jobInfo.Description;
+                newCus.CompanyName = model.jobInfo.CompanyName;
+                newCus.CompanyDescription = model.jobInfo.CompanyDescription;
+
+                newCus.Location = model.jobInfo.Location;
+                newCus.Filled = model.Filled;
+                newCus.LastDate = model.LastDate;
+                _context.SaveChanges();
+
+                return RedirectToAction("updateDashboardSuccess", "UpdateDashboard");
+
+            }
+
+            return View(model);
+
+        }
+
+        //updateSuccess
+
+        [HttpGet]
+        public IActionResult updateDashboardSuccess()
+        {
+            return View();
+        }
+    }
+}
---
+++ Job_Search_App/ViewModels/DashboardViewModel.cs
@@ -0,0 +1,36 @@
+﻿using Job_Search_App.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Job_Search_App.ViewModels
+{
+    public class DashboardViewModel
+    {
+
+        public Job jobInfo { get; set; }
+
+        public string Title { get; set; }
+        [Required, Display(Name = "Job Description", Prompt = "Job Description")]
+        public string Description { get; set; }
+        [Required, Display(Name = "Location", Prompt = "Location")]
+        public string Location { get; set; }
+        [Required(ErrorMessage = "Type is required"), Display(Name = "Type", Prompt = "Type")]
+        public string Type { get; set; }
+        [Required, Display(Name = "Last Date", Prompt = "Last Date")]
+        public DateTime LastDate { get; set; }
+        [Required, Display(Name = "Company Name", Prompt = "Company Name")]
+        public string CompanyName { get; set; }
+        [Required, Display(Name = "Company Description", Prompt = "Company Description")]
+        public string CompanyDescription { get; set; }
+        [Display(Name = "Website", Prompt = "Website")]
+        [Url]
+        public string Website { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public bool Filled { get; set; } = false;
+        public User User { get; set; }
+        public List<Applicant> Applicants { get; set; }
+    }
+}
